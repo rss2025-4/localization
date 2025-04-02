cfe353f1f4821c614489a0a99ef79911aa0b9427
adelenec <adelene@mit.edu>
2025-04-02 15:57:05 -0400

    added paul, eghosa, adelene's scripts

---
+++ localization/particle_filter_adelene.py
@@ -0,0 +1,296 @@
+from localization.sensor_model import SensorModel
+from localization.motion_model import MotionModel
+
+from nav_msgs.msg import Odometry
+from sensor_msgs.msg import LaserScan
+from visualization_msgs.msg import Marker
+from geometry_msgs.msg import PoseWithCovarianceStamped, Pose
+from tf_transformations import euler_from_quaternion, quaternion_from_euler
+from geometry_msgs.msg import TransformStamped
+from tf2_ros import TransformBroadcaster, Buffer, TransformListener
+
+from geometry_msgs.msg import Point
+
+
+from rclpy.node import Node
+import rclpy
+
+assert rclpy
+
+import numpy as np
+
+class ParticleFilter(Node):
+
+    def __init__(self):
+        super().__init__("particle_filter")
+
+        self.declare_parameter('particle_filter_frame', "default")
+        self.particle_filter_frame = self.get_parameter('particle_filter_frame').get_parameter_value().string_value
+
+        #  *Important Note #1:* It is critical for your particle
+        #     filter to obtain the following topic names from the
+        #     parameters for the autograder to work correctly. Note
+        #     that while the Odometry message contains both a pose and
+        #     a twist component, you will only be provided with the
+        #     twist component, so you should rely only on that
+        #     information, and *not* use the pose component.
+        
+        self.declare_parameter('odom_topic', "/odom")
+        self.declare_parameter('scan_topic', "/scan")
+
+        scan_topic = self.get_parameter("scan_topic").get_parameter_value().string_value
+        odom_topic = self.get_parameter("odom_topic").get_parameter_value().string_value
+        
+        self.NUM_PARTICLES = 200
+
+        self.laser_callback_freq = 20 #Hz
+        self.laser_callback_prev_time = 0.0
+
+        self.laser_sub = self.create_subscription(LaserScan, scan_topic,
+                                                  self.laser_callback,
+                                                  1)
+
+        self.odom_sub = self.create_subscription(Odometry, odom_topic,
+                                                 self.odom_callback,
+                                                 1)
+
+        #  *Important Note #2:* You must respond to pose
+        #     initialization requests sent to the /initialpose
+        #     topic. You can test that this works properly using the
+        #     "Pose Estimate" feature in RViz, which publishes to
+        #     /initialpose.
+
+        self.pose_sub = self.create_subscription(PoseWithCovarianceStamped, "/initialpose",
+                                                 self.pose_callback,
+                                                 1)
+        self.pose_estimate_sub = self.create_subscription(Odometry, "/pf/pose/odom",
+                                                 self.pose_estimate_callback,
+                                                 1)                                
+        
+        #  *Important Note #3:* You must publish your pose estimate to
+        #     the following topic. In particular, you must use the
+        #     pose field of the Odometry message. You do not need to
+        #     provide the twist part of the Odometry message. The
+        #     odometry you publish here should be with respect to the
+        #     "/map" frame.
+
+        self.odom_pub = self.create_publisher(Odometry, "/pf/pose/odom", 1)
+
+        # Initialize the models
+        self.motion_model = MotionModel(self)
+        self.sensor_model = SensorModel(self)
+        
+
+        # visualize motion model particles
+        # self.particles_pub = self.create_publisher(Marker, "/motion_model_particles", 1)
+        self.get_logger().info("=============+READY+=============")
+
+        # Implement the MCL algorithm
+        # using the sensor model and the motion model
+        #
+        # Make sure you include some way to initialize
+        # your particles, ideally with some sort
+        # of interactive interface in rviz
+    
+        # from eghosa's code: 
+        self.particles = np.zeros((self.NUM_PARTICLES, 3))
+        self.particle_publisher = self.create_publisher(Marker, "/particle", 1)
+        self.particle_estimate_publisher = self.create_publisher(Marker, "/particle_estimate", 1)
+        
+        # Publish a transformation frame between the map
+        # and the particle_filter_frame.
+        self.tf_broadcaster = TransformBroadcaster(self)
+
+       
+    def laser_callback(self, msg):
+        current_time_msg = self.get_clock().now()
+        seconds, nanoseconds = current_time_msg.seconds_nanoseconds()
+        current_time = seconds + nanoseconds * 1e-9
+        if current_time - self.laser_callback_prev_time < 1.0/self.laser_callback_freq:
+            return
+        pass
+        self.laser_callback_prev_time = current_time
+        observation = msg.ranges
+        self.particle_probabilites = self.sensor_model.evaluate(self.particles, observation)
+        
+        self.particles = self.resample(self.particle_probabilites, self.particles)
+        # get array of most probabilities
+        self.get_pose(self.particles) # publishes pose estimate
+        self.get_logger().info("laser callback")
+
+    def odom_callback(self, msg):
+        # pass
+        x_velocity = msg.twist.twist.linear.x
+        y_velocity = msg.twist.twist.linear.y
+        angular_velocity = msg.twist.twist.angular.z
+        current_time_msg = self.get_clock().now()
+        seconds, nanoseconds = current_time_msg.seconds_nanoseconds()
+        current_time = seconds + nanoseconds * 1e-9
+
+        odometry = self.motion_model.update_odometry(x_velocity, y_velocity, angular_velocity, current_time)
+        self.particles = self.motion_model.evaluate(self.particles, odometry)
+        
+        self.get_pose(self.particles) # publishes pose estimate
+        self.get_logger().info("odom callback")
+    
+    def resample(self, particle_probabilities, particles):
+        if particle_probabilities is None:
+            self.get_logger().info("NOT resampled")
+            return self.particles
+
+        # Create list of options to sample from
+        self.get_logger().info("particle_probabilities_size %s" % len(particle_probabilities))
+        self.get_logger().info("particles_size %s" % len(particles))
+        
+        
+        # particle_probabilities = np.nan_to_num(particle_probabilities, nan=0)
+        particle_probabilities = particle_probabilities / np.sum(particle_probabilities)
+        particle_probabilities = np.nan_to_num(particle_probabilities, nan=0)
+
+        options = np.arange(0,len(particle_probabilities),1)
+
+        self.get_logger().info("hi %s" % particle_probabilities)
+
+        if np.sum(particle_probabilities) <= 1.0:
+            return particles
+            
+        selections = np.random.choice(options, len(particles), p=particle_probabilities)
+        
+        self.get_logger().info("resampled")
+        return particles[selections]
+       
+    def get_pose(self, particles):
+        """
+        Calculate and return the average pose (x, y, theta) of the particles.
+
+        The average x and y positions are calculated as the arithmetic mean,
+        and the average orientation (theta) is calculated using the circular mean.
+
+        Args:
+        particles (list or np.ndarray): List of particles, where each particle is [x, y, theta]
+
+        Returns:
+        np.ndarray: The estimated pose as [x_avg, y_avg, theta_avg]
+        """
+
+        # Convert particles to a numpy array for easier manipulation
+        # particles = np.array(self.particles)
+
+        # Calculate the average x and y positions
+        x_avg = np.mean(particles[:, 0])
+        y_avg = np.mean(particles[:, 1])
+
+        # Circular mean for theta (orientation)
+        theta = particles[:, 2]
+        sin_sum = np.sum(np.sin(theta))
+        cos_sum = np.sum(np.cos(theta))
+        
+        theta_avg = np.arctan2(sin_sum, cos_sum)  # Circular mean to get average angle
+        pose = [x_avg, y_avg, theta_avg]
+        # publish pose 
+        odom_msg = Odometry()
+        odom_msg.pose.pose.position.x = x_avg
+        odom_msg.pose.pose.position.y = y_avg
+        odom_msg.pose.pose.orientation.z = theta_avg
+
+        pose = [x_avg,y_avg,theta_avg]
+        # print("pose estimate", x_avg, y_avg, theta_avg)
+
+        self.odom_pub.publish(odom_msg)
+        # self.get_logger().info("Published pose estimate")
+
+        self.publish_transform(pose)
+        self.get_logger().info("Published transform pf to map")
+
+    def publish_transform(self,pose):
+        [x,y,theta] = pose
+        t = TransformStamped()
+
+        # Read message content and assign it to
+        # corresponding tf variables
+        t.header.stamp = self.get_clock().now().to_msg()
+
+
+        t.header.frame_id = 'map'
+        t.child_frame_id = 'base_link_pf'
+
+        # Turtle only exists in 2D, thus we get x and y translation
+        # coordinates from the message and set the z coordinate to 0
+        t.transform.translation.x = x #msg.pose.pose.position.x
+        t.transform.translation.y = y #msg.pose.pose.position.y
+        t.transform.translation.z = 0.0
+
+        # For the same reason, turtle can only rotate around one axis
+        # and this why we set rotation in x and y to 0 and obtain
+        # rotation in z axis from the message
+        # q = quaternion_from_euler(0, 0, msg.pose.pose.orientation.z)
+        t.transform.rotation.x = 0.0 #q[0]
+        t.transform.rotation.y = 0.0 #q[1]
+        t.transform.rotation.z = theta #q[2]
+        t.transform.rotation.w = 1.0 #q[3]
+
+        # Send the transformation
+        self.tf_broadcaster.sendTransform(t)
+    def pose_estimate_callback(self, msg):
+        """
+        eghosa's pose callback to display particles but on estimate
+        """
+        particle_pts = Marker()
+        particle_pts.type = Marker.POINTS
+        
+        particle_pts.header.frame_id = "/map"
+        
+        particle_pts.scale.x = 0.1
+        particle_pts.scale.y = 0.1
+        particle_pts.color.a = 1.
+        particle_pts.color.r, particle_pts.color.b, particle_pts.color.g = 1.0, 0.69, 0.651
+        
+        for particle in self.particles:
+            p = Point()
+            p.x = particle[0]
+            p.y = particle[1]
+            particle_pts.points.append(p)
+        self.particle_estimate_publisher.publish(particle_pts)
+        self.get_logger().info("published particle estimate")
+
+    def pose_callback(self, msg):
+        """
+        eghosa's pose callback to display initial pose particles (triggered by RVIZ 2d pose estimate)
+        """
+        std_x = 0.5
+        std_y = 0.5
+        std_theta = 0.25
+        
+        x = msg.pose.pose.position.x + 1.0 # try initializing forward a bit
+        y = msg.pose.pose.position.y
+        roll, pitch, yaw = euler_from_quaternion([msg.pose.pose.orientation.x,
+                                                    msg.pose.pose.orientation.y,
+                                                    msg.pose.pose.orientation.z,
+                                                    msg.pose.pose.orientation.w])
+        xs = np.random.normal(x, std_x, self.NUM_PARTICLES)
+        ys = np.random.normal(y, std_y, self.NUM_PARTICLES)
+        thetas = np.random.normal(yaw, std_theta, self.NUM_PARTICLES)
+        self.particles = np.vstack((xs, ys, thetas)).T
+        particle_pts = Marker()
+        particle_pts.type = Marker.POINTS
+        
+        particle_pts.header.frame_id = "/map"
+        
+        particle_pts.scale.x = 0.1
+        particle_pts.scale.y = 0.1
+        particle_pts.color.a = 1.
+        particle_pts.color.r, particle_pts.color.b, particle_pts.color.g = 1.0, 0.69, 0.651
+        
+        for particle in self.particles:
+            p = Point()
+            p.x = particle[0]
+            p.y = particle[1]
+            particle_pts.points.append(p)
+        self.particle_publisher.publish(particle_pts)
+        print("published initial particles")
+
+def main(args=None):
+    rclpy.init(args=args)
+    pf = ParticleFilter()
+    rclpy.spin(pf)
+    rclpy.shutdown()
---
+++ localization/sensor_model_eghosa.py
@@ -0,0 +1,319 @@
+import numpy as np
+from scan_simulator_2d import PyScanSimulator2D
+
+# Try to change to just `from scan_simulator_2d import PyScanSimulator2D`
+# if any error re: scan_simulator_2d occurs
+
+from tf_transformations import euler_from_quaternion
+
+from nav_msgs.msg import OccupancyGrid
+
+import sys
+
+np.set_printoptions(threshold=sys.maxsize)
+
+
+class SensorModel:
+
+    def __init__(self, node):
+
+        self.sensor_node = node
+        node.declare_parameter("map_topic", "default")
+        node.declare_parameter("num_beams_per_particle", 1)
+        node.declare_parameter("scan_theta_discretization", 1.0)
+        node.declare_parameter("scan_field_of_view", 1.0)
+        node.declare_parameter("lidar_scale_to_map_scale", 1.0)
+
+        self.map_topic = (
+            node.get_parameter("map_topic").get_parameter_value().string_value
+        )
+        self.num_beams_per_particle = (
+            node.get_parameter("num_beams_per_particle")
+            .get_parameter_value()
+            .integer_value
+        )
+        self.scan_theta_discretization = (
+            node.get_parameter("scan_theta_discretization")
+            .get_parameter_value()
+            .double_value
+        )
+        self.scan_field_of_view = (
+            node.get_parameter("scan_field_of_view").get_parameter_value().double_value
+        )
+        self.lidar_scale_to_map_scale = (
+            node.get_parameter("lidar_scale_to_map_scale")
+            .get_parameter_value()
+            .double_value
+        )
+
+        ####################################
+        # Adjust these parameters
+        self.alpha_hit = 1  # 0.74
+        self.alpha_short = 0  # 0.07
+        self.alpha_max = 0  # 0.07
+        self.alpha_rand = 0  # 0.12
+        self.sigma_hit = 8.0
+
+        # Your sensor table will be a `table_width` x `table_width` np array:
+        self.table_width = 201
+        ####################################
+
+        node.get_logger().info("%s" % self.map_topic)
+        node.get_logger().info("%s" % self.num_beams_per_particle)
+        node.get_logger().info("%s" % self.scan_theta_discretization)
+        node.get_logger().info("%s" % self.scan_field_of_view)
+
+        # Precompute the sensor model table
+        self.sensor_model_table = np.empty((self.table_width, self.table_width))
+        self.precompute_sensor_model()
+
+        # Create a simulated laser scan
+        self.scan_sim = PyScanSimulator2D(
+            self.num_beams_per_particle,
+            self.scan_field_of_view,
+            0,  # This is not the simulator, don't add noise
+            0.01,  # This is used as an epsilon
+            self.scan_theta_discretization,
+        )
+
+        # Subscribe to the map
+        self.map = None
+        self.map_set = False
+        self.map_subscriber = node.create_subscription(
+            OccupancyGrid, self.map_topic, self.map_callback, 1
+        )
+
+    # def ros_log(node, message):
+    #     node.get_logger().info(message)
+    def precompute_sensor_model(self):
+        """
+        Generate and store a table which represents the sensor model.
+
+        For each discrete computed range value, this provides the probability of
+        measuring any (discrete) range. This table is indexed by the sensor model
+        at runtime by discretizing the measurements and computed ranges from
+        RangeLibc.
+        This table must be implemented as a numpy 2D array.
+
+        Compute the table based on class parameters alpha_hit, alpha_short,
+        alpha_max, alpha_rand, sigma_hit, and table_width.
+
+        args:
+            N/A
+
+        returns:
+            No return type. Directly modify `self.sensor_model_table`.
+        """
+        alpha_hit = self.alpha_hit
+        alpha_short = self.alpha_short
+        alpha_max = self.alpha_max
+        alpha_rand = self.alpha_rand
+        sigma = self.sigma_hit
+
+        z_max = self.table_width - 1
+
+        self.sensor_model_table
+
+        # actual distance d and measured distance z_k^i
+
+        ### Normalize p_hit values across columns
+        def p_hit_z_k_only(z_k, d):
+            """to normalize case 1 only"""
+            # Case 1: probability of detecting known obstacle in map
+            if z_k <= z_max and z_k >= 0:
+                eta = 1
+                p_hit_z_k = (
+                    eta
+                    * 1
+                    / (np.sqrt(2 * np.pi * sigma**2))
+                    * np.exp(-1 / 2 * (z_k - d) ** 2 / sigma**2)
+                )
+            else:
+                p_hit_z_k = 0
+
+            return p_hit_z_k
+
+        # Iterate through the table and fill up with p_hit values, then normalize
+        for z_k in range(self.table_width):  # rows
+            for d in range(self.table_width):  # columns
+                # Compute the probability of measuring j given d
+                self.sensor_model_table[z_k, d] = p_hit_z_k_only(z_k, d)
+        self.sensor_model_table /= np.sum(self.sensor_model_table, axis=0)
+
+        ### Calculate the rest of the cases, add back Case 1, and then normalize entire table
+        def p_z_k_rest(z_k, d):
+            # # Case 1: probability of detecting known obstacle in map
+            # if z_k <= z_max and z_k >=0:
+            #     eta = 1
+            #     p_hit_z_k = eta*1/(np.sqrt(2*np.pi*sigma**2))*np.exp(-1/2*(z_k - d)**2/sigma**2)
+            # else:
+            #     p_hit_z_k = 0
+
+            # Case 2: probability of detecting short measurement (maybe internal lidar reflections)
+            if z_k <= d and z_k >= 0 and d != 0:
+                p_short_z_k = 2 / d * (1 - z_k / d)
+            else:
+                p_short_z_k = 0
+
+            # Case 3: probability of a very large measurement (missed, strange reflection)
+            eps = 1  # 1 pixel
+            if z_k == z_max:
+                p_max_z_k = eps
+            else:
+                p_max_z_k = 0
+
+            # Case 4: probability of random measurement
+            if z_k <= z_max and z_k >= 0:
+                p_random_z_k = 1 / (z_max)
+            else:
+                p_random_z_k = 0
+
+            # Mix the 3 cases
+
+            # alpha_hit*p_hit_z_k +
+            p_z_k = (
+                alpha_short * p_short_z_k
+                + alpha_max * p_max_z_k
+                + alpha_rand * p_random_z_k
+            )
+            return p_z_k
+
+        # Iterate through the table
+        for z_k in range(self.table_width):  # rows
+            for d in range(self.table_width):  # columns
+                # Compute the probability of measuring j given d
+                self.sensor_model_table[z_k, d] = (
+                    p_z_k_rest(z_k, d) + alpha_hit * self.sensor_model_table[z_k, d]
+                )
+
+        # Normalize the table along each column
+        self.sensor_model_table /= np.sum(self.sensor_model_table, axis=0)
+        # self.sensor_model_table = np.nan_to_num(self.sensor_model_table)
+        # raise NotImplementedError
+
+    def evaluate(self, particles, observation):
+        """
+        Evaluate how likely each particle is given
+        the observed scan.
+
+        args:
+            particles: An Nx3 matrix of the form:
+
+                [x0 y0 theta0]
+                [x1 y0 theta1]
+                [    ...     ]
+
+            observation: A vector of lidar data measured
+                from the actual lidar. THIS IS Z_K. Each range in Z_K is Z_K^i
+
+        returns:
+           probabilities: A vector of length N representing
+               the probability of each particle existing
+               given the observation and the map.
+        """
+
+        if not self.map_set:
+            return
+
+        ####################################
+        # TODO
+        # Evaluate the sensor model here!
+        #
+        # You will probably want to use this function
+        # to perform ray tracing from all the particles.
+        # This produces a matrix of size N x num_beams_per_particle
+        node = self.sensor_node
+        scans = self.scan_sim.scan(np.ascontiguousarray(particles)) # ray tracing from all the particles
+        
+        node.get_logger().info("scan %s" % np.size(scans, 0))
+        
+        # convert scan and observation from meters to pixel
+        # by dividing by map resolution and scale
+        scans = np.floor(
+            np.array(scans) / float(self.resolution * self.lidar_scale_to_map_scale)
+        ).astype(int)
+        observation = np.floor(
+            np.array(observation) / float(self.resolution * self.lidar_scale_to_map_scale)
+        ).astype(int)
+
+        N = np.size(scans, 0)
+        # node.get_logger().info("N %s" % N)
+        # node.get_logger().info("converting to pixels scans %s" % np.size(scans, 1))
+        # node.get_logger().info(
+        #     "converting to pixels observation %s" % np.size(observation, 0)
+        # )
+        # then clip to z_max and 0
+        num_beams = np.size(observation, 0)
+        # node.get_logger().info("num_beams %s" % num_beams)
+
+        scans = np.clip(scans, 0, self.table_width - 1)
+        observation = np.clip(observation, 0, self.table_width - 1)
+
+        # node.get_logger().info("clipped to pixels scans %s" % np.size(scans, 1))
+        # node.get_logger().info(
+        #     "clipped to pixels observation %s" % np.size(observation, 0)
+        # )
+
+        particle_probabilites = np.zeros((len(particles), 1))
+        for j in range(N):
+            # scan is a list of the form [d1, d2, d3, ...]
+            # where each d_i is the distance from the particle to the closest obstacle
+            # in the direction of the i-th beam
+            scan = scans[j, :]  # get scan (some j-th row)
+            # node.get_logger().info("in for loop, scan %s" % np.size(scan, 0))
+            total_p_z_k = 1  # initialize a total probability for each particle
+            for i in range(num_beams):  # each measured distance z_k_i
+                # use precomputed table to get the probability of each beam
+                # given the distance from the map
+                # and the distance from the laser scan
+                z_k_i = observation[i]
+                d_i = scan[i]
+                p_z_k_i = self.sensor_model_table[z_k_i, d_i]
+
+                # likelihood of each particle is a product of the likelihoods of each beam
+                total_p_z_k = total_p_z_k * p_z_k_i
+            # node.get_logger().info("made it through beams")
+            particle_probabilites[j, 0] = total_p_z_k
+                
+        # node.get_logger().info("sum of particle probabilities %s" % np.sum(particle_probabilites))
+        # node.get_logger().info("all particle probabilities %s" % particle_probabilites)
+        ####################################
+        
+        
+        return particle_probabilites.flatten()
+
+    def map_callback(self, map_msg):
+        # Convert the map to a numpy array
+        self.map = np.array(map_msg.data, np.double) / 100.0
+        self.map = np.clip(self.map, 0, 1)
+
+        self.resolution = map_msg.info.resolution
+
+        # Convert the origin to a tuple
+        origin_p = map_msg.info.origin.position
+        origin_o = map_msg.info.origin.orientation
+        origin_o = euler_from_quaternion(
+            (origin_o.x, origin_o.y, origin_o.z, origin_o.w)
+        )
+        origin = (origin_p.x, origin_p.y, origin_o[2])
+
+        # Initialize a map with the laser scan
+        self.scan_sim.set_map(
+            self.map,
+            map_msg.info.height,
+            map_msg.info.width,
+            map_msg.info.resolution,
+            origin,
+            0.5,
+        )  # Consider anything < 0.5 to be free
+
+        # Make the map set
+        self.map_set = True
+
+        print("Map initialized")
+
+
+# print("Sensor model initialized")
+# if __name__ == "__main__":
+# sensor_model = SensorModel()
+# print(SensorModel.sensor_model_table[0,:])
